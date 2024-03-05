5e605fd3bde5e4d63973387c4117fc395bb3e8c8
Enrique Alberto Navia <anavia_n@outlook.com>
2024-03-05 13:10:34 -0600

    Terminamos modulos

--- CursoFernandoHDir/CursoFernandoH/bases/src/app/app.module.ts
+++ CursoFernandoHDir/CursoFernandoH/bases/src/app/app.module.ts
@@ -3,19 +3,17 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
+import { CounterModule } from './counter/counter.module';
+import { HeroModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent,
-    HeroComponent,
-    ListComponent
   ],
   imports: [
     BrowserModule,
+    CounterModule,
+    HeroModule,
     AppRoutingModule
   ],
   providers: [
