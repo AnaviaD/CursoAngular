8f9204a90901fac3601a2b58c0032c0a7b5ee984
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2024-03-06 10:48:11 -0600

    emitir eventos

---
+++ CursoFernandoHDir/CursoFernandoH/bases/src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css',
+})
+export class DbzListComponent {
+
+  @Input()
+  public characterList: Character[] = [{
+    name:'Trunks',
+    power: 10
+  }]
+}
