a87f43cd64f72ad3d5d242cb5912ce9fa9014f20
Enrique Alberto Navia <anavia_n@outlook.com>
2024-03-07 15:48:29 -0600

    Vista de componente funcional

---
+++ CursoGifsDir/AppGifs/demoapp/src/app/gifs/components/card-lilst/card-lilst.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-card-lilst',
+  templateUrl: './card-lilst.component.html',
+})
+export class CardLilstComponent {
+
+}
--- CursoGifsDir/AppGifs/demoapp/src/app/gifs/gifs.module.ts
+++ CursoGifsDir/AppGifs/demoapp/src/app/gifs/gifs.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { CardLilstComponent } from './components/card-lilst/card-lilst.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent,
+    SearchBoxComponent,
+    CardLilstComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HomePageComponent
   ]
 })
 export class GifsModule { }
