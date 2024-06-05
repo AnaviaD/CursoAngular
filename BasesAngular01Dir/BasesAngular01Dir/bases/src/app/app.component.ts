a88fe40aeada8c5c65a41b4675cea784fe160416
Enrique Alberto Navia <anavia_n@outlook.com>
2024-06-05 17:13:26 -0600

    Adding new project

---
+++ BasesAngular01Dir/BasesAngular01Dir/bases/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  public title: String = 'Mi primera app de Angular';
+  public counter: number = 10;
+
+  increaseBy(value: number):void{
+    this.counter+=value;
+  }
+
+  reset():void{
+    this.counter = 10;
+  }
+}
