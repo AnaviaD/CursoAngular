871ca25f6024d85cdfb54a67ec3c188f4e6fe1f9
Enrique Alberto Navia <anavia_n@outlook.com>
2024-06-24 19:53:54 -0600

    review Component

--- BasesAngular01Dir/BasesAngular01Dir/bases/src/app/counter/counter.component.ts
+++ BasesAngular01Dir/BasesAngular01Dir/bases/src/app/counter/counter.component.ts
@@ -4,29 +4,25 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-counter',
   template: `
+  <hr>
   <h3>Counter: {{counter}}</h3>
 
-
-  <button (click)="increaseBy(1)" >+1</button>
-  <button (click)="reset()">Reset</button>
-  <button (click)="increaseBy(-1)">-1</button>
-
+  <button (click)="increaseBy(+1)"> +1 </button>
+  <button (click)="resetFunc()"> Reset </button>
+  <button (click)="increaseBy(-1)"> -1 </button>
   `,
 })
+
 export class CounterComponent{
-  constructor()
-  {
-
-  }
-
-  public title: String = 'Mi primera app de Angular';
   public counter: number = 10;
 
-  increaseBy(value: number):void{
-    this.counter+=value;
+  resetFunc(): void
+  {
+    this.counter = 10;
   }
 
-  reset():void{
-    this.counter = 10;
+  increaseBy(value: number):void
+  {
+    this.counter += value;
   }
 }
