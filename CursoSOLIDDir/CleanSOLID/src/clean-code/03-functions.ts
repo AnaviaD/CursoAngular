13b18a2931a53eec41aa3a791983aae1c963c079
Enrique Alberto Navia <anavia_n@outlook.com>
2024-06-26 00:29:42 -0600

    Adding function and interface

---
+++ CursoSOLIDDir/CleanSOLID/src/clean-code/03-functions.ts
@@ -0,0 +1,55 @@
+(() => {
+
+    interface movie {
+        title:          string;
+        description:    string;
+        rating:         number;
+        cast:           string[];
+    }
+
+    interface actor {
+        fullName:   string, 
+        birthdate:  Date
+    }
+
+    // función para obtener información de una película por Id
+    function getMovieById( movieId: string ) {
+        console.log({ movieId });
+    }
+
+    // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+    function getMovieCastById( id: string ) {
+        console.log({ id });
+    }
+
+    // funcion para obtener el bio del actor por el id
+    function getActorById( ActorId: string ) {
+        console.log({ ActorId });
+    }
+    
+    // Crear una película
+    function createMovie({title, description, rating, cast} : movie) {
+        console.log({ title, description, rating, cast });
+    }
+
+    // Crea un nuevo actor
+    function createActor({fullName, birthdate}: actor ): boolean {
+        
+        // tarea asincrona para verificar nombre
+        // ..
+        // ..
+        if ( fullName === 'fernando' ) return false;
+
+        console.log('Crear actor');
+        return true;        
+
+    }
+
+    
+
+
+})();
+
+
+
+
