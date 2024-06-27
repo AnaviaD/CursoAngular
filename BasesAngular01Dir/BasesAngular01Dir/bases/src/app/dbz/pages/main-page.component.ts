e4fa29c3435d2c8252c7964e0043b30058b5a46f
Enrique Alberto Navia <anavia_n@outlook.com>
2024-06-26 23:18:21 -0600

    Adding input

--- BasesAngular01Dir/BasesAngular01Dir/bases/src/app/dbz/pages/main-page.component.ts
+++ BasesAngular01Dir/BasesAngular01Dir/bases/src/app/dbz/pages/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -6,5 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class MainPageComponent{
+
+  public characters: Character[] = [{
+      name: 'Krillin',
+      power: 500
+    },{
+      name: 'Goku',
+      power: 10000
+    },
+    {
+      name    :'Trunks',
+      power   :100000
+    },
+    {
+      name    :'Vegeta',
+      power   :7000
+    }];
 
 }
