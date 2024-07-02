56d60aecbbbeb852db7a4b7d68c36ef4990166c4
Enrique Alberto Navia <anavia_n@outlook.com>
2024-07-02 17:13:31 -0600

    Adding Id

--- BasesAngular01Dir/BasesAngular01Dir/bases/src/app/dbz/services/dbz.service.ts
+++ BasesAngular01Dir/BasesAngular01Dir/bases/src/app/dbz/services/dbz.service.ts
@@ -1,28 +1,40 @@
 import { Injectable } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+import { v4 as uuid } from 'uuid';
 
 @Injectable({providedIn: 'root'})
 export class DbzService{
 
   public characters: Character[] = [{
+    id: uuid(),
     name: 'Krillin',
     power: 500
   },{
+    id: uuid(),
     name: 'Goku',
     power: 10000
   },
   {
+    id: uuid(),
     name    :'Trunks',
     power   :100000
   },
   {
+    id: uuid(),
     name    :'Vegeta',
     power   :7000
   }];
 
-  onNewCharacterPadre(caracter:Character): void
+  onNewCharacterPadre({name, power}:Character): void
   {
-    this.characters.push(caracter);
+
+    const newCharacter: Character ={
+      id: uuid(),
+      name,
+      power,
+    }
+
+    this.characters.push(newCharacter);
   }
 
   onDeletePadre(index:number): void{
