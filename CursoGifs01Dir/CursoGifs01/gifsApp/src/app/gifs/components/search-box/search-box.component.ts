fbe0b3f9110b40d722b6eb22fcc6a9b4515e2c52
Enrique Alberto Navia <anavia_n@outlook.com>
2024-07-04 19:08:41 -0600

    adding card list and sidebarcomponent

--- CursoGifs01Dir/CursoGifs01/gifsApp/src/app/gifs/components/search-box/search-box.component.ts
+++ CursoGifs01Dir/CursoGifs01/gifsApp/src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
@@ -6,9 +7,29 @@ import { Component } from '@angular/core';
   <h5>Buscar: </h5>
   <input type="text"
   class="form-control"
-  placeholder="Buscar gifs...">
+  placeholder="Buscar gifs..."
+  (keyup.enter)="searchTag()"
+  #txtTagInput
+  >
   `,
 })
 export class SearchBoxComponent {
 
+  constructor(private gifsService: GifsService){
+
+  }
+
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  // searchTag( newTag: string ): void {
+  searchTag(): void {
+    const newTag = this.tagInput.nativeElement.value;
+
+    this.gifsService.searchTag(newTag);
+
+    this.tagInput.nativeElement.value = '';
+
+    // console.log(newTag);
+  }
 }
