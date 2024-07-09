e8fa9ab915f49ecb976de1d102ef727664f1c9d1
Enrique Alberto Navia <anavia_n@outlook.com>
2024-07-09 16:50:57 -0600

    Adding interface, structure support Gifs

--- CursoGifs01Dir/CursoGifs01/gifsApp/src/app/gifs/services/gifs.service.ts
+++ CursoGifs01Dir/CursoGifs01/gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,14 +1,18 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  private _tagsHistory: string[] = [];
-  private apiKey: string = 'hIPm7c4DiN6VaMfwszx68EnDBxu3RMND';
+  public gifList            : Gif[] = [];
+  private _tagsHistory      : string[] = [];
+  private apiKey            : string = 'hIPm7c4DiN6VaMfwszx68EnDBxu3RMND';
+  private serviceUrl        : string = 'https://api.giphy.com/v1/gifs';
 
 
   get tagsHistory(){
@@ -29,7 +33,25 @@ export class GifsService {
   searchTag(tag: string):void {
 
     if (tag.length === 0) return;
+
     this.organizeHistory(tag);
+
+    const params = new HttpParams()
+    .set('api_key', this.apiKey)
+    .set('limit', '10')
+    .set('q', tag)
+
+    this.http.get<SearchResponse>(`${ this.serviceUrl }/search`, {params: params})
+    .subscribe((resp) => {
+
+      this.gifList = resp.data;
+
+      console.log({ gifs: this.gifList });
+    })
+
+    // const resp = await fetch('https://api.giphy.com/v1/gifs/trending?api_key=hIPm7c4DiN6VaMfwszx68EnDBxu3RMND')
+    // .then( resp => resp.json())
+    // .then( data => console.log(data));
 
   }
 
