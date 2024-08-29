c3aabce8ebbea0bcca58eb0b1aa377f09dd70ff0
Enrique Alberto Navia <anavia_n@outlook.com>
2024-08-28 18:58:29 -0600

    Adding B bone

--- MyPageDir/MyPage/myPage/src/app/shared/components/sidebar/sidebar.component.ts
+++ MyPageDir/MyPage/myPage/src/app/shared/components/sidebar/sidebar.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'shared-sidebar',
   templateUrl: './sidebar.component.html',
-  styles: ``
+  styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
 
