5f7c644ec10df8272494531f078332f6c4b0882f
Enrique Alberto Navia <anavia_n@outlook.com>
2024-08-29 17:47:28 -0600

    Starting the navBar

--- MyPageDir/MyPage/myPage/src/app/shared/components/sidebar/sidebar.component.ts
+++ MyPageDir/MyPage/myPage/src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { navbarData } from './nav-data';
 
 @Component({
   selector: 'shared-sidebar',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+
+  collapsed_ = false;
+  navData =  navbarData;
+
 
 }
