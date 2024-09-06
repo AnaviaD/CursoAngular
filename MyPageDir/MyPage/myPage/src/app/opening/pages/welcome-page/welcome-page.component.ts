8a9fafd0f2a3d8834db340a75a5df6bbacede8b9
Enrique Alberto Navia <anavia_n@outlook.com>
2024-09-06 01:49:35 -0600

    Change box to btn

--- MyPageDir/MyPage/myPage/src/app/opening/pages/welcome-page/welcome-page.component.ts
+++ MyPageDir/MyPage/myPage/src/app/opening/pages/welcome-page/welcome-page.component.ts
@@ -41,6 +41,10 @@ export class WelcomePageComponent implements AfterViewInit {
 
 
 
+    // Estos serian los objetos interactuables del mapa
+    const cubes: Array<THREE.Mesh<THREE.BoxGeometry>> = [];
+    let remainingObjects: THREE.Object3D[] = [...cubes]; // Copia inicial de objetos disponibles
+
     // Variables globales para la animación de la cámara
     const rotationSpeed = (2 * Math.PI) / 60000;
     let angle = 0;
@@ -53,13 +57,14 @@ export class WelcomePageComponent implements AfterViewInit {
     let previousTime = 0;
 
 
+
     // Crear la escena
     const scene = new THREE.Scene();
     scene.background = new THREE.Color(0x87ceeb);
 
     // Crear la cámara
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set(0, 70, 0);
+    camera.position.set(0, 50, 0);
     camera.lookAt(0, 0, 0);
 
     // Añadir controles de órbita
@@ -69,6 +74,16 @@ export class WelcomePageComponent implements AfterViewInit {
     controls.maxPolarAngle = THREE.MathUtils.degToRad(70); // 70 grados
     controls.update();
 
+    // Añadir luz a la escena
+    const ambientLight = new THREE.AmbientLight(0x404040);
+    scene.add(ambientLight);
+
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLight.position.set(10, 20, 10);
+    scene.add(directionalLight);
+
+
+    //#region    LoadingManager(LoadingScreen)
     const loadingManager = new THREE.LoadingManager();
     //inicio de la carga
     loadingManager.onStart = function(url, item, total){
@@ -87,6 +102,7 @@ export class WelcomePageComponent implements AfterViewInit {
     loadingManager.onError = function(url){
       console.error(`Error loading   ${url}`);
     }
+    //#endregion
 
     // Creamos el loader
     const loader = new GLTFLoader(loadingManager);
@@ -137,8 +153,6 @@ export class WelcomePageComponent implements AfterViewInit {
     plane.rotation.x = -Math.PI / 2;
     scene.add(plane);
 
-    // Función para generar cubos aleatoriamente en el plano
-    const cubes: Array<THREE.Mesh<THREE.BoxGeometry>> = [];
     const cubeGeometry = new THREE.BoxGeometry(2, 2, 2);
     const cubeMaterial = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
 
@@ -153,16 +167,16 @@ export class WelcomePageComponent implements AfterViewInit {
         cubes.push(cube);
     }
 
-    // Añadir luz a la escena
-    const ambientLight = new THREE.AmbientLight(0x404040);
-    scene.add(ambientLight);
-
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-    directionalLight.position.set(10, 20, 10);
-    scene.add(directionalLight);
-
+    //Cubo boton
+    const followerCube = new THREE.Mesh(
+      new THREE.BoxGeometry(1, 1, 1),
+      new THREE.MeshBasicMaterial({ color: 0xff0000 })
+    );
+    scene.add(followerCube);
 
 
+
+    /*==============  Animation   ============== */
     // Función para animar la escena
     function animate(time: number) {
       requestAnimationFrame(animate);
@@ -171,27 +185,65 @@ export class WelcomePageComponent implements AfterViewInit {
       previousTime = time; // Actualiza el tiempo previo para el siguiente frame
 
 
+
+      //#region    CameraMovents
+      //Va a checar si el objetivo esta cerca de la camara
+      limitCameraMovement();
+      updateFollowerCube();
+
       if (isAnimating) {
         // Interpola la posición de la cámara hacia la posición objetivo
         cameraOnClickAnimation();
       }
-
-      //Va a checar si el objetivo esta cerca de la camara
-      if ((camera.position.distanceTo(targetPosition) < 15.15) || (camera.position.distanceTo(targetPosition) < 10.15) && !isAnimating) {
-        posicionateCamera();
-      }else{
-        isPosicion = false;
-      }
-
       if (isRotating) {
         rotateCameraAnimation(deltaTime);
       }else{
         isRotating = false;
       }
+      //#endregion
 
       renderer.render(scene, camera);
     }
 
+
+    //#region    CubeBtn
+    function updateFollowerCube() {
+      // Obtén la dirección en la que la cámara está mirando
+      const cameraDirection = new THREE.Vector3();
+      camera.getWorldDirection(cameraDirection);
+
+      // Calcula la nueva posición para el cubo a 5 unidades de distancia frente a la cámara
+      const distance = 7; // Distancia desde la cámara
+      const newPosition = new THREE.Vector3().copy(camera.position).add(cameraDirection.multiplyScalar(distance));
+
+      // Ajusta el cubo para que esté más arriba en la pantalla (por ejemplo, 2 unidades más arriba en el eje Y)
+      const verticalOffset = 3; // Ajusta este valor para mover el cubo más arriba o abajo
+      newPosition.y += verticalOffset;
+
+      // Actualiza la posición del cubo
+      followerCube.position.copy(newPosition);
+    }
+    //#endregion
+
+    //#region    limitCameraMovement
+    function limitCameraMovement() {
+      // Limitar el valor de Y entre 0 y 50
+
+      if (camera.position.y < 0) {
+        camera.position.y = 0;
+      }
+      if (camera.position.y > 30) {
+        camera.position.y = 30;
+      }
+      if ((camera.position.distanceTo(targetPosition) < 15.15) || (camera.position.distanceTo(targetPosition) < 10.15) && !isAnimating) {
+        posicionateCamera();
+      }else{
+        isPosicion = false;
+      }
+    }
+    //#endregion
+
+    //#region    posicionateCamera
     function posicionateCamera(){
       const x = targetPosition.x + 12 * Math.sin(rotationSpeed);
       const z = targetPosition.z + 12 * Math.cos(rotationSpeed);
@@ -231,7 +283,9 @@ export class WelcomePageComponent implements AfterViewInit {
       // Inicia la rotación
       animatePosition();
     }
+    //#endregion
 
+    //#region    rotateCameraAnimation
     function rotateCameraAnimation(deltaTime: number){
       // Define el radio y el ángulo de rotación
       let radius = 12;
@@ -268,7 +322,9 @@ export class WelcomePageComponent implements AfterViewInit {
       // Inicia la rotación
       animateRotation();
     }
+    //#endregion
 
+    //#region    cameraOnClickAnimation
     function cameraOnClickAnimation(){
 
       controls.target.set(targetPosition.x, targetPosition.y, targetPosition.z);
@@ -287,8 +343,10 @@ export class WelcomePageComponent implements AfterViewInit {
         isPosicion = true;
       }
     }
+    //#endregion
 
     // Detectar clic en los cubos y mover la cámara
+    //#region    onMouseClick
     function onMouseClick(event: { clientX: number; clientY: number; }) {
       const mouse = new THREE.Vector2(
           (event.clientX / window.innerWidth) * 2 - 1,
@@ -320,6 +378,46 @@ export class WelcomePageComponent implements AfterViewInit {
 
           isAnimating = true; // Activa la animación
       }
+
+      if (raycaster.intersectObject(followerCube)){
+        console.log('btn Cube clicado ');
+        const randomObject = selectRandomObject();
+
+        targetPosition.set(
+              randomObject!.position.x,
+              1,
+              randomObject!.position.z
+          );
+
+          targetLook.set(
+              randomObject!.position.x,
+              1,
+              randomObject!.position.z
+          );
+
+          isAnimating = true; // Activa la animación
+      }
+    }
+    //#endregion
+
+
+    // Función para seleccionar un objeto aleatorio sin repetir
+    function selectRandomObject(): THREE.Object3D | null {
+      if (remainingObjects.length === 0) {
+        // Si ya se han seleccionado todos, restablece la lista
+        remainingObjects = [...cubes];
+      }
+
+      // Selecciona un índice aleatorio de los objetos restantes
+      const randomIndex = Math.floor(Math.random() * remainingObjects.length);
+
+      // Extrae el objeto seleccionado
+      const selectedObject = remainingObjects[randomIndex];
+
+      // Elimina el objeto seleccionado de los restantes
+      remainingObjects.splice(randomIndex, 1);
+
+      return selectedObject;
     }
 
     function onWheelDown(){
