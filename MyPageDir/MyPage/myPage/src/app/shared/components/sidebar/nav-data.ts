ec1cd5aec226ccae10107508393a212219730075
Enrique Alberto Navia <anavia_n@outlook.com>
2024-10-03 16:45:04 -0600

    Finishing first ver

--- MyPageDir/MyPage/myPage/src/app/shared/components/sidebar/nav-data.ts
+++ MyPageDir/MyPage/myPage/src/app/shared/components/sidebar/nav-data.ts
@@ -1,44 +1,46 @@
 export const navbarData =
 [
   {
-    routeLink: '',
-    icon: 'fal fa-home',
-    label:'Home'
-  },
-  {
     routeLink: 'opening/welcome',
     icon: 'fal fa-truck',
-    label:'MainPage'
-  },
-  {
-    routeLink: 'opening/card',
-    icon: '',
-    svg:  'assets/mySVGs/address-card.svg',
-    label:'MainPage'
-  },
-  {
-    routeLink: 'opening/first',
-    icon: 'fal fa-car',
-    label:'MainPage'
-  },
-  {
-    routeLink: 'opening/GF',
-    icon: 'fal fa-school',
-    label:'MainPage'
+    label:'MainPage',
+    svg:''
   },
   {
     routeLink: 'opening/cat',
     icon: 'fal fa-gamepad',
     label:'MainPage'
   },
-  {
-    routeLink: 'opening/car',
-    icon: 'fal fa-rocket',
-    label:'MainPage'
-  },
-  {
-    routeLink: 'opening/transport',
-    icon: 'fal fa-ship',
-    label:'MainPage'
-  }
 ];
+
+// {
+//   routeLink: 'opening/card',
+//   icon: '',
+//   svg:  'assets/mySVGs/address-card.svg',
+//   label:'MainPage'
+// },
+// {
+//   routeLink: '',
+//   icon: 'fal fa-home',
+//   label:'Home'
+// },
+// {
+//   routeLink: 'opening/first',
+//   icon: 'fal fa-car',
+//   label:'MainPage'
+// },
+// {
+//   routeLink: 'opening/GF',
+//   icon: 'fal fa-school',
+//   label:'MainPage'
+// },
+// {
+//   routeLink: 'opening/car',
+//   icon: 'fal fa-rocket',
+//   label:'MainPage'
+// },
+// {
+//   routeLink: 'opening/transport',
+//   icon: 'fal fa-ship',
+//   label:'MainPage'
+// }
