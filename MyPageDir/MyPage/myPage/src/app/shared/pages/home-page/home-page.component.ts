0117f471eb7f34efaed03586aea76907767b8434
Enrique Alberto Navia <anavia_n@outlook.com>
2024-10-05 23:22:17 -0600

    set positions

--- MyPageDir/MyPage/myPage/src/app/shared/pages/home-page/home-page.component.ts
+++ MyPageDir/MyPage/myPage/src/app/shared/pages/home-page/home-page.component.ts
@@ -60,17 +60,20 @@ export class HomePageComponent {
 
     // Crear la cámara y posicionarla
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set(0, 0, 5); // Alejar la cámara en el eje Z
+    camera.position.set(0, 80, -310); // Alejar la cámara en el eje Z
+    camera.lookAt(0, 150, 0);
 
     // Añadir controles de órbita
-    const controls = new OrbitControls(camera, renderer.domElement);
-    controls.minPolarAngle = THREE.MathUtils.degToRad(30); // 30 grados
-    controls.maxPolarAngle = THREE.MathUtils.degToRad(70); // 70 grados
-    controls.update();
+    // const controls = new OrbitControls(camera, renderer.domElement);
+    // controls.minPolarAngle = THREE.MathUtils.degToRad(70); // 30 grados
+    // controls.maxPolarAngle = THREE.MathUtils.degToRad(90); // 70 grados
+
+    // controls.update();
 
     // Añadir una luz para iluminar el cubo
-    const light = new THREE.DirectionalLight(0xffffff, 1);
-    light.position.set(10, 10, 10);
+    const light = new THREE.DirectionalLight(0xffffff,1);
+    light.position.set(0, 20, -350);
+    light.lookAt(0, 0, 0);
     scene.add(light);
     //#endregion
 
@@ -141,6 +144,35 @@ export class HomePageComponent {
       console.error('Error al cargar el modelo GLTF:', error);
     });
 
+    // Cargar el modelo
+    loader.load(
+    'assets/glts/welcome.glb',
+    (gltf) => {
+      const original = gltf.scene;
+      original.position.set(0, 0, -300);
+      original.scale.set(0.4, 0.4, 0.4);
+      original.rotation.y = 3.1;
+
+      scene.add(original);
+
+      // Crear un mixer para el objeto original
+      mixer = new THREE.AnimationMixer(original);
+      gltf.animations.forEach(clip => {
+        const action = mixer.clipAction(clip);
+        action.timeScale = 1;
+        action.play();
+      });
+
+      // Añadir los mixers al array para ser actualizados
+      mixerArray.push(mixer);
+      // mixerArray.push(mixer1);
+    },
+    undefined,
+    (error) => {
+      console.error('Error al cargar el modelo GLTF:', error);
+    });
+
+
     //#endregion
 
 
@@ -152,7 +184,7 @@ export class HomePageComponent {
     /*==============  Animation   ============== */
     const animate = (time: number) => {
       requestAnimationFrame(animate);
-      controls.update();
+      // controls.update();
 
       const deltaTime = (time - previousTime) * 0.0001; // Convierte el tiempo delta a segundos
       previousTime = time;
