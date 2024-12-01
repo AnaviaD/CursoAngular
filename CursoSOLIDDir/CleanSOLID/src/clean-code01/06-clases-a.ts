31f27730af2ef4488594cc1d3218accf40624371
Enrique Alberto Navia <anavia_n@outlook.com>
2024-11-30 22:10:43 -0600

    short ver

--- CursoSOLIDDir/CleanSOLID/src/clean-code01/06-clases-a.ts
+++ CursoSOLIDDir/CleanSOLID/src/clean-code01/06-clases-a.ts
@@ -3,17 +3,26 @@
     type Gender = 'M' | 'F';
 
     class Person{
-        public name: string;
-        public gender: Gender;
-        public birthdate: Date;
-
-        constructor(name: string, gender: Gender, birthdate: Date)
-        {
-            this.name = name;
-            this.gender = gender;
-            this.birthdate = birthdate;
-        }
+        constructor(
+            public name: string, 
+            public gender: Gender, 
+            public birthdate: Date
+        ) {}
     }
+
+    // Long ver
+    // class Person{
+    //     public name: string;
+    //     public gender: Gender;
+    //     public birthdate: Date;
+
+    //     constructor(name: string, gender: Gender, birthdate: Date)
+    //     {
+    //         this.name = name;
+    //         this.gender = gender;
+    //         this.birthdate = birthdate;
+    //     }
+    // }
 
     const newPerson = new Person('Fenando', 'M', new Date('2014-01-21'));
 
