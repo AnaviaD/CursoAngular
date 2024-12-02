ab40c70e97577ae2a0737ad00d3b32c8144e82e5
Enrique Alberto Navia <anavia_n@outlook.com>
2024-12-02 16:52:26 -0600

    solve homework not the right way

--- CursoSOLIDDir/CleanSOLID/src/clean-code01/07-tarea-b.ts
+++ CursoSOLIDDir/CleanSOLID/src/clean-code01/07-tarea-b.ts
@@ -6,32 +6,49 @@
     type HtmlType = 'input'|'select'|'textarea'|'radio';
    
     interface HtmlElementProps {
-        id: string;
-        type: HtmlType;
+        id      : string;
+        type    : HtmlType;
     }
 
     class HtmlElement {
-        constructor(
-            public id: string,
-            public type: HtmlType,
-        ) {}
-    }
-
-
-
-    class InputAttributes extends HtmlElement {
-        constructor(
-            public value: string,
-            public placeholder: string,
-            id: string,
-        ) {
-            super(id, 'input');
+        public id: string;
+        public type: HtmlType;
+        constructor({ id, type }: HtmlElementProps) 
+        {
+            this.id = id;
+            this.type = type;
         }
     }
 
-    class InputEvents extends InputAttributes {
-        constructor( value: string, placeholder: string, id: string ) {
-            super( value, placeholder, id );
+    interface InputAttributesProps{
+        value           : string;
+        placeholder     : string;
+    }
+
+    class InputAttributes {
+        
+        public value: string;
+        public placeholder: string;
+        
+        constructor({ value, placeholder } : InputAttributesProps) {
+            this.value = value;
+            this.placeholder = placeholder;
+        }
+    }
+
+    interface InputElementProps{
+        value           : string;
+        placeholder     : string;
+        id              : string;
+    }
+
+    class InputElement{
+        public htmlElement: HtmlElement;
+        public inputAttributes : InputAttributes;
+
+        constructor({ value, placeholder, id }: InputElementProps){
+            this.htmlElement = new HtmlElement({id, type : 'input'});
+            this.inputAttributes = new InputAttributes({value, placeholder});
         }
 
         setFocus() {};
@@ -40,11 +57,24 @@
         removeValue() {};
     }
 
-
+    
     //? Idea para la nueva clase InputElement
-
-    const nameField = new InputEvents('Fernando', 'Enter first name', 'txtName');
-
+    
+    const nameField = new InputElement({ value: 'Fernando', placeholder: 'Enter first name', id: 'txtName'});
+    
     console.log({ nameField });
-
+    
 })()
+
+
+
+// class InputEvents extends InputAttributes {
+//     constructor( value: string, placeholder: string, id: string ) {
+//         super( value, placeholder, id );
+//     }
+
+//     setFocus() {};
+//     getValue() {};
+//     isActive() {};
+//     removeValue() {};
+// }
