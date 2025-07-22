cc9c164ced3e3867ba00534657442926938a21b7
Enrique Alberto Navia <anavia_n@outlook.com>
2025-07-21 18:22:58 -0600

    Start Again

---
+++ BasesAngular-03Dir/BasesAngular-03/bases/src/app/pages/counter/counter.component.ts
@@ -0,0 +1,27 @@
+import { Component, signal } from "@angular/core";
+
+
+
+@Component({
+  templateUrl: 'counter.component.html'
+})
+export class CounterPageComponent {
+
+  counter = 10;
+  counterSignal = signal(10);
+
+  increaseBy(value: number){
+    this.counter += 1;
+    this.counterSignal.update((current) => current + value);
+  }
+
+  decreaseBy(value: number){
+    this.counter -= 1;
+  }
+
+  resetBy()
+  {
+    this.counter = 10;
+    this.counterSignal.set(10);
+  }
+}
