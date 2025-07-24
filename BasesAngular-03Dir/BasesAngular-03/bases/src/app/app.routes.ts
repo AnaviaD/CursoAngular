0af4b0d355837804c0284e91ffa419cc16638d67
Enrique Alberto Navia <anavia_n@outlook.com>
2025-07-23 19:14:30 -0600

    23 session

--- BasesAngular-03Dir/BasesAngular-03/bases/src/app/app.routes.ts
+++ BasesAngular-03Dir/BasesAngular-03/bases/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { CounterPageComponent } from './pages/counter/counter-page.component';
 import { MultiplyPageComponent } from './pages/counter/multiply-page.component';
 import { HeroPageComponent } from './pages/hero/hero-page.component';
+import { DragonballPageComponent } from './pages/dragonball/dragonball-page/dragonball-page';
 
 export const routes: Routes = [
   {
@@ -15,5 +16,13 @@ export const routes: Routes = [
   {
     path:'hero',
     component:HeroPageComponent
-  }
+  },
+  {
+    path: 'dragonball',
+    component:DragonballPageComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  },
 ];
