03b57dade3ebc711c877aeee424e520e3b2e087c
Enrique Alberto Navia <anavia_n@outlook.com>
2025-08-08 18:02:36 -0600

    Routes works

--- BasesAngular-03Dir/BasesAngular-03/gifs-app02/gifs-app/src/app/gifs/components/gifs-side-menu-options/gifs-side-menu-options.component.ts
+++ BasesAngular-03Dir/BasesAngular-03/gifs-app02/gifs-app/src/app/gifs/components/gifs-side-menu-options/gifs-side-menu-options.component.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOption {
+  label:      string;
+  icon:       string;
+  route:      string;
+  sublabel:   string;
+}
+
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './gifs-side-menu-options.component.html',
 })
-export class GifsSideMenuOptionsComponent { }
+export class GifsSideMenuOptionsComponent {
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      sublabel: 'Gifs Populares',
+      route: '/dashboard/trending'
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      sublabel: 'Buscar gifs',
+      route: '/dashboard/search'
+    }
+  ]
+}
