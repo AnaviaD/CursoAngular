5def6e81d80426ef9e13dcde0a58a85b775a0038
W <105769948+AnaviaD@users.noreply.github.com>
2026-04-25 20:14:10 -0600

    add session

---
+++ CursoJSModerno/Curso JS Moderno/02-Variables/js/01-app.js
@@ -0,0 +1,7 @@
+var producto = 'Monitor de 24 pulgadas'
+
+producto = 'Monitor de 19 pulgadas'
+
+var categoria = 'computadoras',
+    marca = 'Marca Famosa',
+    calificacion = 4;
---
+++ CursoJSModerno/Curso JS Moderno/03-Strings/js/01-app.js
@@ -0,0 +1,10 @@
+const producto = "monitor de 20 pulgadas"
+const producto02 = String("Monitor de 24 pulgadas");
+
+const producto03 = new String("Monitor 27 Pulgadas");
+
+
+
+console.log(producto)
+console.log(producto02)
+console.log(producto03)
