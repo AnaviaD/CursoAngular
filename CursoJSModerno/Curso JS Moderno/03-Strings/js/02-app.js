2ba8c62fadb8a3cd5fe3bdc5b0bc0b3826f376ff
W <105769948+AnaviaD@users.noreply.github.com>
2026-04-25 22:28:56 -0600

    Add session

---
+++ CursoJSModerno/Curso JS Moderno/03-Strings/js/02-app.js
@@ -0,0 +1,7 @@
+const producto = 'Monitor 20 Pulgadas';
+
+console.log(producto.length)
+
+console.log(producto.indexOf('Monitor'))
+console.log(producto.indexOf('Pulgadas'))
+console.log(producto.includes('Monitor'))
---
+++ CursoJSModerno/Curso JS Moderno/03-Strings/js/05-app.js
@@ -0,0 +1,14 @@
+const producto = 'Monitor 20 pulgadas ';
+
+console.log(producto);
+console.log(producto.replace('pulgadas', '"'));
+console.log(producto.replace('Monitor', 'Monitor Curvo'));
+
+//slice para cortar
+console.log(producto.slice(0, 10))
+console.log(producto.slice(8))
+
+
+//Alternativa a Slice
+console.log(producto.substring(0, 10));
+console.log(producto.substring(2, 1));
