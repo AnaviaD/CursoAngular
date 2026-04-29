4dbf8bb87cdc48c1a40d7928660a5d145d89f248
W <105769948+AnaviaD@users.noreply.github.com>
2026-04-28 19:49:04 -0600

    finish session

---
+++ CursoJSModerno/Curso JS Moderno/07-Objetos/js/09-app.js
@@ -0,0 +1,19 @@
+"use strict"
+
+const producto = {
+    nombre: "Monitor de 20 Pulgadas",
+    precio: 300,
+    disponible : true
+}
+
+const medidas = {
+    peso: '1Kg',
+    medida: '1m'
+}
+
+console.log(producto)
+console.log(medidas)
+
+const resultado = Object.assign(producto, medidas);
+
+console.log(resultado)
