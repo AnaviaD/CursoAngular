1c5ccb0ffb521fcaefe1076fe6f5871847f01081
W <105769948+AnaviaD@users.noreply.github.com>
2026-04-30 15:26:29 -0600

    add class session

--- TypescriptBases/typescript-intro/src/main.ts
+++ TypescriptBases/typescript-intro/src/main.ts
@@ -1,6 +1,7 @@
 import './style.css'
-import './topics/06-function-destructuring'
-import './topics/07-import'
+// import './topics/06-function-destructuring'
+// import './topics/07-import'
+import './topics/08-classes'
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
---
+++ TypescriptBases/typescript-intro/src/topics/08-classes.ts
@@ -0,0 +1,39 @@
+
+
+export class Person {
+    // public name : string;
+    // public address: string;
+    constructor(
+        public name:    string = '',
+        public address: string = ''
+    ){}
+}
+
+// export class Hero extends Person {
+//     constructor(
+//         public alterEgo: string,
+//         public age: number,
+//         public realName: string
+//     ){
+//         super(realName, 'New York');
+//     }
+// }
+
+export class Hero {
+    
+    constructor(
+        public alterEgo: string,
+        public age: number,
+        public realName: string,
+        public person: Person
+    ){
+        // this.person = new Person(realName)
+    }
+}
+
+const tony = new Person('Tony Stark', 'New York')
+
+// const ironman = new Person('Iron man', 'Costa Rica');
+const ironman = new Hero('Iron man', 45, 'Tony', tony);
+
+console.log(ironman)
