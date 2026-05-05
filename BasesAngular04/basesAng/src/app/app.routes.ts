677ce9fa35433c42726f056a88dea47a0827dfed
W <105769948+AnaviaD@users.noreply.github.com>
2026-05-04 23:16:58 -0600

    adding counter

--- BasesAngular04/basesAng/src/app/app.routes.ts
+++ BasesAngular04/basesAng/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { counterPageComponent } from './pages/counter/counter-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: '',
+    component: counterPageComponent
+  }
+];
