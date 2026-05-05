be5a7eb62a8632b3ba8b404698ba4a33f6aae037
W <105769948+AnaviaD@users.noreply.github.com>
2026-05-05 17:47:36 -0600

    Add operadores

---
+++ CursoJSModerno/Curso JS Moderno/08-Arrays/js/01-app.js
@@ -0,0 +1,9 @@
+const numeros = [10, 20, 30];
+
+const meses = ['Enero', 'Febrero', 'Marzo'];
+
+const meses01 = new Array('Enero', 'Febrero', 'Marzo')
+
+console.log(numeros)
+console.log(meses)
+console.log(meses01)
