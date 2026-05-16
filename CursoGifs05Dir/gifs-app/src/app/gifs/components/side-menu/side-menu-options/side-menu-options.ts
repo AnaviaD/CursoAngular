c3da8728d85e37c95dd02e9be295b1d4e3531f2b
W <105769948+AnaviaD@users.noreply.github.com>
2026-05-16 17:58:16 -0600

    add components side menu

--- CursoGifs05Dir/gifs-app/src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
+++ CursoGifs05Dir/gifs-app/src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
@@ -1,8 +1,33 @@
 import {  Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+interface MenuOption{
+  label: string;
+  subLabel: string;
+  router: string;
+  icon: string;
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.html',
 })
-export class SideMenuOptions {}
+export class SideMenuOptions {
+
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs Pupulares',
+      router: '/dashboard/trending'
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      subLabel: 'Buscar Gifs',
+      router: '/dashboard/search'
+    },
+  ]
+
+}
