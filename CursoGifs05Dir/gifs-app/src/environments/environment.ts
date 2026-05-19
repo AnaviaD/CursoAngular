921ecdb0f94e33568121875bc6b464a7686ba5dd
W <105769948+AnaviaD@users.noreply.github.com>
2026-05-18 21:21:18 -0600

    Add api key

--- CursoGifs05Dir/gifs-app/src/environments/environment.ts
+++ CursoGifs05Dir/gifs-app/src/environments/environment.ts
@@ -2,5 +2,7 @@ export const environment = {
   production: true,
   companyName: 'Gifs',
   companyName2: 'App',
-  companySlogan: 'Maneja tus gifs'
+  companySlogan: 'Maneja tus gifs',
+
+  apiKey: 'Eg3l3J4yzwienWzVlZ1DY2k2EzfxuCI6'
 };
