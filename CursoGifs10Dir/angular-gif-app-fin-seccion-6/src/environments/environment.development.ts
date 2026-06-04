34a00275b21532752c474d3716af6da1ef6d409b
W <105769948+AnaviaD@users.noreply.github.com>
2026-06-03 19:04:01 -0600

    adding url api keys

--- CursoGifs10Dir/angular-gif-app-fin-seccion-6/src/environments/environment.development.ts
+++ CursoGifs10Dir/angular-gif-app-fin-seccion-6/src/environments/environment.development.ts
@@ -5,6 +5,8 @@ export const environment = {
   companySlogan: 'Maneja tus gifs',
 
   // ApiKeys
+  giphyApiKey: 'Eg3l3J4yzwienWzVlZ1DY2k2EzfxuCI6',
+  urlApi: 'https://api.giphy.com/v1'
 
   // URLS
 };
