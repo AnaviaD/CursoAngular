8d907a78aade321fd8aa913b1b1c260c2edf21bf
W <105769948+AnaviaD@users.noreply.github.com>
2026-06-04 21:58:10 -0600

    adding history

--- CursoGifs10Dir/angular-gif-app-fin-seccion-6/src/app/gifs/pages/search-page/search-page.component.ts
+++ CursoGifs10Dir/angular-gif-app-fin-seccion-6/src/app/gifs/pages/search-page/search-page.component.ts
@@ -1,6 +1,7 @@
-import {  Component, inject } from '@angular/core';
+import {  Component, inject, signal } from '@angular/core';
 import { GifListComponent } from "../../components/gif-list/gif-list.component";
 import { GifService } from '../../services/gifs.service';
+import { myGif } from '../../interfaces/myGif.interface';
 
 @Component({
   selector: 'app-search-page',
@@ -9,9 +10,12 @@ import { GifService } from '../../services/gifs.service';
 })
 export default class SearchPageComponent {
 
+  searchGifs = signal<myGif[]>([]);
   gifServ = inject(GifService)
 
   onSearch(query: string){
-    this.gifServ.searchGifs(query)
+    this.gifServ.searchGifs(query).subscribe((resp) => {
+      this.searchGifs.set(resp)
+    })
   }
 }
--- CursoGifs10Dir/angular-gif-app-fin-seccion-6/src/app/gifs/services/gifs.service.ts
+++ CursoGifs10Dir/angular-gif-app-fin-seccion-6/src/app/gifs/services/gifs.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable, signal } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
 import { environment } from '@environments/environment';
 import { GiphyResponse } from '../interfaces/giphy.interfaces';
 import { myGif } from '../interfaces/myGif.interface';
 import { gifMapper } from '../mappers/gifMapper.mapper';
+import { map, tap } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class GifService {
@@ -12,6 +13,9 @@ export class GifService {
   env = environment
   trendingGifs = signal<myGif[]>([])
   trendingGifsLoading = signal(true)
+
+  searchHistory = signal<Record<string, myGif[]>>({});
+  searchHistoryKeys = computed(() => Object.keys(this.searchHistory()))
 
   constructor() {
     this.loadTrendingGifs()
@@ -32,16 +36,22 @@ export class GifService {
   }
 
   searchGifs(query: string){
-    this.http.get<GiphyResponse>(`${this.env.urlApi}/gifs/search`,{
+    return this.http.get<GiphyResponse>(`${this.env.urlApi}/gifs/search`,{
       params:{
         api_key: this.env.giphyApiKey,
         limit: 20,
         q:query
       },
-    }).subscribe((resp) =>{
-      const gifs = gifMapper.mapGiphyItemsToGifArray(resp.data)
-      console.log(gifs)
-    })
+    }).pipe(
+      map(({data}) => data),
+      map((items) => gifMapper.mapGiphyItemsToGifArray(items)),
+      tap(items => {
+        this.searchHistory.update(history => ({
+          ...history,
+          [query.toLocaleUpperCase()]:items,
+        }))
+      })
+    );
   }
 
 }
