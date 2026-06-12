ed0913f83c4b3c87fb8b130d86b187411df56fba
W <105769948+AnaviaD@users.noreply.github.com>
2026-06-11 19:26:18 -0600

    trending gifs working

--- CursoGifs14Dir/GifApp14/src/app/gifs/services/gifs.service.ts
+++ CursoGifs14Dir/GifApp14/src/app/gifs/services/gifs.service.ts
@@ -1,24 +1,30 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '@environments/environment';
+import { myGif } from '../interfaces/myGif.interface';
+import { giphyMapper } from '../mappers/giphyMapper.mapper';
+import { GiphyResponse } from '../interfaces/giphyGif.interface';
 
 @Injectable({providedIn: 'root'})
 export class GifService {
 
   private http = inject(HttpClient)
+  trendingGifs = signal<myGif[]>([])
 
   constructor() {
     this.loadTrendingGifs()
   }
 
   loadTrendingGifs(){
-    this.http.get(`${environment.urlApi}/gifs/trending`, {
+    this.http.get<GiphyResponse>(`${environment.urlApi}/gifs/trending`, {
       params:{
         api_key: environment.giphyApiKey,
         limit: 20
       }
     }).subscribe((resp) =>{
-      console.log(resp)
+      const gifs = giphyMapper.giphyArrayToFigArray(resp.data)
+      this.trendingGifs.set(gifs)
+      console.log(gifs)
     })
   }
 
