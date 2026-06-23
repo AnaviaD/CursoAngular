9df8b332839ebff2ea7400418f924ff816bee491
W <105769948+AnaviaD@users.noreply.github.com>
2026-06-22 21:56:52 -0600

    add cache on search page

--- CursoGifs18Dir/angular-gif-app-18/src/app/gifs/services/gifService.service.ts
+++ CursoGifs18Dir/angular-gif-app-18/src/app/gifs/services/gifService.service.ts
@@ -1,16 +1,19 @@
-import { inject, Injectable, signal } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
 import { myGif } from '../interfaces/myGif.interface';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '@environments/environment';
 import { giphyToGifMapper } from '../Mapper/gifMapper.mapper';
 import { GiphyResponse } from '../interfaces/giphyResponse.interface';
-import { map } from 'rxjs';
+import { map, tap } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class GifService {
 
   trendingGifs = signal<myGif[]>([]);
   private http = inject(HttpClient)
+
+  searchHistory = signal<Record<string, myGif[]>>({})
+  searchHistoryKeys = computed(() => Object.keys(this.searchHistory()))
 
   constructor() {
     this.loadingTrendingGifs()
@@ -37,7 +40,13 @@ export class GifService {
         q: query
       }
     }).pipe(
-      map((items) => giphyToGifMapper.giphyArrayToGifArray(items.data))
+      map((items) => giphyToGifMapper.giphyArrayToGifArray(items.data)),
+      tap(items =>{
+        this.searchHistory.update(history =>({
+          ...history,
+          [query.toLowerCase()]: items,
+        }))
+      })
     )
   }
 
