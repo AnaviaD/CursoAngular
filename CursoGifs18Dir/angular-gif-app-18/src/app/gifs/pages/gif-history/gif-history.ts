e1249ebb4d2fc0bb9d1c95087ba8e94e3865c266
W <105769948+AnaviaD@users.noreply.github.com>
2026-06-22 22:19:26 -0600

    add cache and hisory page

--- CursoGifs18Dir/angular-gif-app-18/src/app/gifs/pages/gif-history/gif-history.ts
+++ CursoGifs18Dir/angular-gif-app-18/src/app/gifs/pages/gif-history/gif-history.ts
@@ -1,8 +1,17 @@
-import {  Component } from '@angular/core';
+import {  Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-gif-history',
   imports: [],
   templateUrl: './gif-history.html',
 })
-export default class GifHistory {}
+export default class GifHistory {
+
+  query = toSignal(
+    inject(ActivatedRoute).params.pipe(map((params) => params['query']))
+  )
+
+}
