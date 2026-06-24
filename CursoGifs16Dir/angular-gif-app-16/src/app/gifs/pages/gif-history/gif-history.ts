02270eb7decad93240cf33f6153638565bc9ae89
W <105769948+AnaviaD@users.noreply.github.com>
2026-06-23 21:36:39 -0600

    adding history routes

---
+++ CursoGifs16Dir/angular-gif-app-16/src/app/gifs/pages/gif-history/gif-history.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gif-history',
+  imports: [],
+  templateUrl: './gif-history.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class GifHistory {}
