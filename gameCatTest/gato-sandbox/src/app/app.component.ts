a16cf02ab02d7fbf3d7819965bc55b411c46f1bb
W <105769948+AnaviaD@users.noreply.github.com>
2026-07-02 21:11:24 -0600

    add game canvas

--- gameCatTest/gato-sandbox/src/app/app.component.ts
+++ gameCatTest/gato-sandbox/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GameCanvasComponent } from "./game/components/game-canvas/game-canvas.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GameCanvasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
