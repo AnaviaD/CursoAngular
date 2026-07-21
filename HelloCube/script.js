83ba41ed655b5cd9baba279f306707d5776e2342
W <105769948+AnaviaD@users.noreply.github.com>
2026-07-20 19:27:41 -0600

    adding scene and mesh

--- HelloCube/script.js
+++ HelloCube/script.js
@@ -1,3 +1,13 @@
 import * as THREE from './three/three.js-master/build/three.module.min.js'
 
-console.log(THREE)
+const scene = new THREE.scene();
+
+//Mesh
+const geometry = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshBasicMaterial({color: "purple"})
+
+const mesh = new THREE.Mesh(geometry, material)
+
+scene.add(mesh);
+
+//camera
