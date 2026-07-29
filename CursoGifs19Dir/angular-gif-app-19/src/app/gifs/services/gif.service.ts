fa07a2f9505849929b2f789bde77ae2fd16b5cd1
W <105769948+AnaviaD@users.noreply.github.com>
2026-07-28 20:13:03 -0600

    start search gifs function

--- CursoGifs19Dir/angular-gif-app-19/src/app/gifs/services/gif.service.ts
+++ CursoGifs19Dir/angular-gif-app-19/src/app/gifs/services/gif.service.ts
@@ -28,5 +28,15 @@ export class GifService {
     });
   }
 
+  searchGifs(query:string){
+    this.http.get<GiphyResponse>(`${environment.urlApi}/gifs/search`,{
+      params:{
+        api_key: environment.giphyApiKey,
+        limit: 20,
+        q:query
+      }
+    })
+  }
+
 
 }
