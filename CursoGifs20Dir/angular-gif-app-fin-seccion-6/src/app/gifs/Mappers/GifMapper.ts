6cdb9de49e5e0cccb21fb688e3529c672434f5ca
W <105769948+AnaviaD@users.noreply.github.com>
2026-07-29 17:10:01 -0600

    Add interface and mappers

--- CursoGifs20Dir/angular-gif-app-fin-seccion-6/src/app/gifs/Mappers/GifMapper.ts
+++ CursoGifs20Dir/angular-gif-app-fin-seccion-6/src/app/gifs/Mappers/GifMapper.ts
@@ -1,5 +1,16 @@
-export class GiffMapper{
-  static giphyItemToGfItem(){
+import { GiphyItem, GiphyResponse } from "../interfaces/giphy.interface";
+import { myGif } from "../interfaces/myGif.interface";
 
+export class GiffMapper{
+  static giphyItemToGfItem(item: GiphyItem): myGif{
+    return {
+      id: item.id,
+      title: item.title,
+      url: item.images.original.url
+    }
+  }
+
+  static giphyArrayToGifArray(items: GiphyItem[]):myGif[]{
+    return items.map(this.giphyItemToGfItem)
   }
 }
