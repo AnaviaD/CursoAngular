e25c16debb8f769788e1d0d805befdb0f68f9bef
W <105769948+AnaviaD@users.noreply.github.com>
2026-07-28 19:29:36 -0600

    Add search page

--- CursoGifs19Dir/angular-gif-app-19/src/app/gifs/pages/search-page/search-page.component.ts
+++ CursoGifs19Dir/angular-gif-app-19/src/app/gifs/pages/search-page/search-page.component.ts
@@ -1,9 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { GifListComponent } from "../../components/gif-list/gif-list.component";
+import { GifService } from '../../services/gif.service';
 
 @Component({
   selector: 'app-search-page',
-  imports: [],
+  imports: [GifListComponent],
   templateUrl: './search-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class SearchPageComponent {}
+export default class SearchPageComponent {
+
+  gifServis = inject(GifService)
+
+  searchGifByName(name: string){
+    console.log(name)
+  }
+}
