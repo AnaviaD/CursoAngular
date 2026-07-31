8475644a4f112da8eac1271851d8612f341dd5e9
W <105769948+AnaviaD@users.noreply.github.com>
2026-07-30 19:10:48 -0600

    add environment and service

---
+++ CursoGifs21Dir/angular-gif-app-fin-seccion-6/src/app/gifs/services/gif.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { myGif } from '../interfaces/myGif.interface';
+import { environment } from '@environments/environment';
+import { gifMapper } from '../Mappers/gifMapper.mapper';
+import { GiphyResponse } from '../interfaces/giphy.interface';
+
+@Injectable({providedIn: 'root'})
+export class GifService {
+
+  private http = inject(HttpClient)
+  trendingGifs = signal<myGif[]>([])
+
+  constructor() { }
+
+  loadingTrendingGifs(){
+    this.http.get<GiphyResponse>(`${environment.urlApi}/gifs/trending`,{
+      params: {
+        api_key: environment.giphyApiKey,
+        limit: 10
+      }
+    }).subscribe((data) =>{
+      const gifs = gifMapper.giphyArrayToGifArray(data.data)
+      this.trendingGifs.set(gifs)
+    })
+  }
+}
