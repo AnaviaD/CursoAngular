2bb0f68d509f72bcd9242d1600e463ca410891b1
W <105769948+AnaviaD@users.noreply.github.com>
2026-08-06 18:35:30 -0600

    gifs by search working and record gifs

--- CursoGifs23Dir/angular-gif-app-fin-seccion-6/src/app/gifs/pages/search-page/search-page.component.ts
+++ CursoGifs23Dir/angular-gif-app-fin-seccion-6/src/app/gifs/pages/search-page/search-page.component.ts
@@ -1,9 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { GifListComponent } from "../../components/gif-list/gif-list.component";
+import { GifService } from '../../service/gifService.service';
+import { myGif } from '../../Interface/myGif.interface';
 
 @Component({
   selector: 'app-search-page',
-  imports: [],
+  imports: [GifListComponent],
   templateUrl: './search-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class SearchPageComponent {}
+export default class SearchPageComponent {
+
+  gifServis = inject(GifService)
+  searchGifs = signal<myGif[]>([])
+
+  gifBySearch(query: string){
+    this.gifServis.searchGifsByName(query).subscribe((items) => this.searchGifs.set(items))
+
+  }
+}
--- CursoGifs23Dir/angular-gif-app-fin-seccion-6/src/app/gifs/service/gifService.service.ts
+++ CursoGifs23Dir/angular-gif-app-fin-seccion-6/src/app/gifs/service/gifService.service.ts
@@ -1,15 +1,20 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable, signal } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
 import { myGif } from '../Interface/myGif.interface';
 import { environment } from '@environments/environment';
 import { giphyMapper } from '../Mappers/giphyMapper.mapper';
 import { GiphyResponse } from '../Interface/GiphyResponse.interface';
+import { map, tap } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class GifService {
 
   private http = inject(HttpClient)
   trendingGifs = signal<myGif[]>([])
+  trendingGifsLoading = signal(true)
+
+  searchHistory = signal<Record<string, myGif[]>>({})
+  searchHistoryKeys = computed(()=> Object.keys(this.searchHistory()))
 
   constructor() {
     this.loadTrendingGifs()
@@ -25,6 +30,24 @@ export class GifService {
       const gifs = giphyMapper.GiphyArrayToGifArray(items.data)
       this.trendingGifs.set(gifs)
     })
+  }
+
+  searchGifsByName(query:string){
+    return this.http.get<GiphyResponse>(`${environment.urlApi}/gifs/search`,{
+      params:{
+        api_key: environment.giphyApiKey,
+        q: query,
+        limit: 10
+      }
+    }).pipe(
+      map((items) => giphyMapper.GiphyArrayToGifArray(items.data)),
+      tap((items) => {
+        this.searchHistory.update( history => ({
+          ...history,
+          [query.toLocaleLowerCase()]: items
+        }))
+      })
+    )
   }
 
 }
