c400466fdb6b84ffddc4e07d3d1651711203cb7d
W <105769948+AnaviaD@users.noreply.github.com>
2026-08-09 18:04:54 -0600

    add mapper

---
+++ CursoGifs24Dir/angular-gif-app-fin-seccion-6/src/app/gifs/Mappers/giphyMapper.mapper.ts
@@ -0,0 +1,16 @@
+import { GiphyItem } from "../interfaces/giphyResponse.interface";
+import { myGif } from "../interfaces/myGif.interface";
+
+export class giphyMapper{
+  static giphyItemToGif(item: GiphyItem): myGif{
+    return {
+      id: item.id,
+      title: item.title,
+      url: item.images.original.url
+    }
+  }
+
+  static giphyArrayToGifArray(items: GiphyItem[]): myGif[]{
+    return items.map(this.giphyItemToGif)
+  }
+}
