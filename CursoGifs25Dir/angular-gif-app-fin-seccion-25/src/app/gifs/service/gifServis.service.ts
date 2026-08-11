f00a0163fd8a69bc8f8bf3aa9793c32cdc80b3c6
W <105769948+AnaviaD@users.noreply.github.com>
2026-08-10 18:16:45 -0600

    add service load trending gifs

--- CursoGifs25Dir/angular-gif-app-fin-seccion-25/src/app/gifs/service/gifServis.service.ts
+++ CursoGifs25Dir/angular-gif-app-fin-seccion-25/src/app/gifs/service/gifServis.service.ts
@@ -1,7 +1,31 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { myGif } from '../interface/myGif.interface';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '@environments/environment';
+import { GifMapper } from '../Mapper/GifMapper.mapper';
+import { GiphyResponse } from '../interface/GiphyResponse.interface';
 
 @Injectable({providedIn: 'root'})
 export class GifService {
-  constructor() { }
+
+  private http =inject(HttpClient)
+  trendingGifs = signal<myGif[]>([])
+
+  constructor() {
+    this.loadTrendingGifs()
+  }
+
+  loadTrendingGifs(){
+    this.http.get<GiphyResponse>(`${environment.urlApi}/gifs/trending`,{
+      params: {
+        api_key: environment.giphyApiKey,
+        limit: 10
+      }
+    }).subscribe((items) => {
+      const gifs = GifMapper.arrayGiphyToGif(items.data)
+      this.trendingGifs.set(gifs)
+    })
+  }
+
 
 }
